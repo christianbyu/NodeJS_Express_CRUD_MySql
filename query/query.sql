a8c237a316cc51d8cf691e8111d37f26b4fefef2
pc aing <christianbayu@google.com>
2023-03-12 20:30:01 +0700

    update for dockerize

---
+++ query/query.sql
@@ -0,0 +1,3 @@
+Create table mahasiswa ( nim varchar(15) Primary Key, Nama varchar ( 50), foto varchar ( 255 ) , tanggal_lahir date, alamat varchar(255));
+
+insert into mahasiswa (nim, Nama, foto, tanggal_lahir, alamat) values ('00002','Uwu','','2023-01-01','Jakardah');
