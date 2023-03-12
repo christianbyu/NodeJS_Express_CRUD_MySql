ecda1b80cf4d33bb36f417319efc1c1a84338e27
pc aing <christianbayu@google.com>
2023-03-12 20:54:19 +0700

    update query

--- query/query.sql
+++ query/query.sql
@@ -1,3 +1,7 @@
+create database akademik;
+
+use akademik;
+
 Create table mahasiswa ( nim varchar(15) Primary Key, Nama varchar ( 50), foto varchar ( 255 ) , tanggal_lahir date, alamat varchar(255));
 
 insert into mahasiswa (nim, Nama, foto, tanggal_lahir, alamat) values ('00002','Uwu','','2023-01-01','Jakardah');
